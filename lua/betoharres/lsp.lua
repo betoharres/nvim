8c8eba2c43226c034cbefc1b190aaf55bf38c718
Alberto Harres <betoharres@gmail.com>
2025-02-07 04:40:28 -0300

    finish(apparently)

--- lua/betoharres/lsp.lua
+++ lua/betoharres/lsp.lua
@@ -6,9 +6,9 @@ vim.opt.signcolumn = 'yes'
 -- This should be executed before you configure any language server
 local lspconfig_defaults = require('lspconfig').util.default_config
 lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+'force',
+lspconfig_defaults.capabilities,
+require('cmp_nvim_lsp').default_capabilities()
 )
 
 -- This is where you enable features that only work
@@ -77,72 +77,96 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
     -- Simple tab complete
     -- ['<Tab>'] = cmp.mapping(function(fallback)
-    --   local col = vim.fn.col('.') - 1
-    --
-    --   if cmp.visible() then
-    --     cmp.select_next_item({behavior = 'select'})
-    --   elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    --     fallback()
-    --   else
-    --     cmp.complete()
-    --   end
-    -- end, {'i', 's'}),
-    --
-    -- -- Go to previous item
-    -- ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    --
-    --
-    --
+      --   local col = vim.fn.col('.') - 1
+      --
+      --   if cmp.visible() then
+      --     cmp.select_next_item({behavior = 'select'})
+      --   elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+      --     fallback()
+      --   else
+      --     cmp.complete()
+      --   end
+      -- end, {'i', 's'}),
+      --
+      -- -- Go to previous item
+      -- ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+      --
+      --
+      --
 
-  }),
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-  preselect = 'item',
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-})
-
--- format on save
-local buffer_autoformat = function(bufnr)
-  local group = 'lsp_autoformat'
-  vim.api.nvim_create_augroup(group, {clear = false})
-  vim.api.nvim_clear_autocmds({group = group, buffer = bufnr})
-
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    buffer = bufnr,
-    group = group,
-    desc = 'LSP format on save',
-    callback = function()
-      -- note: do not enable async formatting
-      vim.lsp.buf.format({async = false, timeout_ms = 10000})
-    end,
+    }),
+    snippet = {
+      expand = function(args)
+        vim.snippet.expand(args.body)
+      end,
+    },
+    preselect = 'item',
+    completion = {
+      completeopt = 'menu,menuone,noinsert'
+    },
   })
-end
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    local id = vim.tbl_get(event, 'data', 'client_id')
-    local client = id and vim.lsp.get_client_by_id(id)
-    if client == nil then
-      return
-    end
 
-    -- make sure there is at least one client with formatting capabilities
-    if client.supports_method('textDocument/formatting') then
-      buffer_autoformat(event.buf)
-    end
-  end
-})
+  -- format on save
+  local buffer_autoformat = function(bufnr)
+    local group = 'lsp_autoformat'
+    vim.api.nvim_create_augroup(group, {clear = false})
+    vim.api.nvim_clear_autocmds({group = group, buffer = bufnr})
 
--- format on leader f
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    local opts = {buffer = event.buf}
-    vim.keymap.set({'n', 'x'}, '<leader>f', function()
-      vim.lsp.buf.format({async = false, timeout_ms = 10000})
-    end, opts)
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = bufnr,
+      group = group,
+      desc = 'LSP format on save',
+      callback = function()
+        -- note: do not enable async formatting
+        vim.lsp.buf.format({async = false, timeout_ms = 10000})
+      end,
+    })
   end
-})
+  vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(event)
+      local id = vim.tbl_get(event, 'data', 'client_id')
+      local client = id and vim.lsp.get_client_by_id(id)
+      if client == nil then
+        return
+      end
+
+      -- make sure there is at least one client with formatting capabilities
+      if client.supports_method('textDocument/formatting') then
+        buffer_autoformat(event.buf)
+      end
+    end
+  })
+
+  -- format on leader f
+  vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(event)
+      local opts = {buffer = event.buf}
+      vim.keymap.set({'n', 'x'}, '<leader>f', function()
+        vim.lsp.buf.format({async = false, timeout_ms = 10000})
+      end, opts)
+    end
+  })
+
+  require'nvim-treesitter.configs'.setup {
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = {
+          'rust',
+          'javascript',
+          'typescript',
+          'lua',
+          'gdscript',
+          'go',
+          'html',
+          'css',
+          'templ',
+          'vim',
+          'markdown',
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
