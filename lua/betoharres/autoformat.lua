51cbcacf9ff6e34ce9bdf9c7156dc6c685643968
Alberto Harres <betoharres@gmail.com>
2025-03-18 01:44:38 -0300

    add avante and improve treesitter

--- lua/betoharres/autoformat.lua
+++ lua/betoharres/autoformat.lua
@@ -1,40 +1,45 @@
 local buffer_autoformat = function(bufnr)
-  local group = 'lsp_autoformat'
-  vim.api.nvim_create_augroup(group, { clear = false })
-  vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    buffer = bufnr,
-    group = group,
-    desc = 'LSP format on save',
-    callback = function()
-      -- Never request typescript-language-server for formatting
-      vim.lsp.buf.format {
-        async = false,
-        timeout_ms = 10000,
-        -- ts_ls formatting sucks
-        filter = function(client) return client.name ~= "ts_ls" end
-      }
-    end,
-  })
+	local group = "lsp_autoformat"
+	vim.api.nvim_create_augroup(group, { clear = false })
+	vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		buffer = bufnr,
+		group = group,
+		desc = "LSP format on save",
+		callback = function()
+			-- Never request typescript-language-server for formatting
+			vim.lsp.buf.format({
+				async = false,
+				timeout_ms = 10000,
+				-- ts_ls formatting sucks
+				filter = function(client)
+					return client.name ~= "ts_ls"
+				end,
+			})
+		end,
+	})
 end
 
 require("mason-null-ls").setup({
-  ensure_installed = { "stylua", "jq", "golines", "prettierd", "eslint", "rubocop" }
+	ensure_installed = { "stylua", "jq", "golines", "prettierd", "eslint", "rubocop" },
 })
 
 local null_ls = require("null-ls")
 null_ls.setup({
-  sources = {
-    -- js format
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.golines.with({
-      extra_args = { "--max-len=80" } -- Adjust maximum line length as needed
-    }),
-    require("none-ls.diagnostics.eslint")
-  },
-  on_attach = function(_, buf)
-    buffer_autoformat(buf)
-  end
+	sources = {
+		-- js format
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.rubocop,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.golines.with({
+			extra_args = { "--max-len=80" }, -- Adjust maximum line length as needed
+		}),
+		require("none-ls.diagnostics.eslint"),
+	},
+	on_attach = function(_, buf)
+		buffer_autoformat(buf)
+	end,
 })
 
 -- default neovim way to autoformat; commented out because of null-ls
