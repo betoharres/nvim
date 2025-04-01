814951bf200831c3a6fa9b29c5007b67224e6fac
Alberto Harres <betoharres@gmail.com>
2025-04-01 01:57:37 -0300

    attempt to make auto tags work with tsx

--- lua/betoharres/functions.lua
+++ lua/betoharres/functions.lua
@@ -50,6 +50,29 @@ end
 
 require("lualine").setup()
 
+require("nvim-ts-autotag").setup({
+	opts = {
+		-- Defaults
+		enable_close = true, -- Auto close tags
+		enable_rename = true, -- Auto rename pairs of tags
+		enable_close_on_slash = false, -- Auto close on trailing </
+	},
+
+	-- Also override individual filetype configs, these take priority.
+	-- Empty by default, useful if one of the "opts" global settings
+	-- doesn't work well in a specific filetype
+	-- per_filetype = {
+	-- 	["html"] = {
+	-- 		enable_close = true, -- Auto close tags
+	-- 		enable_rename = true, -- Auto rename pairs of tags
+	-- 		enable_close_on_slash = false, -- Auto close on trailing </
+	-- 	},
+	-- },
+	aliases = {
+		["typescriptreact"] = "html",
+	},
+})
+
 -- vim.api.nvim_create_autocmd("WinClosed", {
 -- 	callback = function(tbl)
 -- 		if vim.api.nvim_buf_is_valid(tbl.buf) then
