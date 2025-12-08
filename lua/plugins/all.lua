ab5602bf67a6f908c8d6398834c05fac3ee2e468
Alberto Harres <betoharres@gmail.com>
2025-12-08 16:32:43 -0300

    fix: debugger for golang

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -19,7 +19,41 @@ return {
 	{ "nvim-lua/plenary.nvim" },
 
 	-- debugger
-	{ "mfussenegger/nvim-dap" },
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			dapui.setup()
+			require("dap-go").setup()
+
+			dap.configurations.go = {
+				{
+					type = "go",
+					name = "Debug file (stop on entry)",
+					request = "launch",
+					program = "${file}",
+					stopOnEntry = true,
+				},
+			}
+
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
 
 	-- lsp
 	{ "neovim/nvim-lspconfig" },
